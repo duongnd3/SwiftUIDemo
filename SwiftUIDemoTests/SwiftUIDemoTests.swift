f536874a65001bcf07bf66a3d4c81a0c5c0bee8e
duongnd2 <nguyenduongnd2@gmail.com>
2020-10-17 23:39:35 +0900

    Commit UnitTest

---
+++ SwiftUIDemoTests/SwiftUIDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUIDemoTests.swift
+//  SwiftUIDemoTests
+//
+//  Created by Nguyen Dinh Duong on 2020/10/17.
+//  Copyright © 2020 NDBlack. All rights reserved.
+//
+
+import XCTest
+
+class SwiftUIDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
