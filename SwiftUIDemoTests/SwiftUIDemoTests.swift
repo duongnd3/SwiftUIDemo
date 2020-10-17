95cf2b4721c979e5da607a6fb9aa34030830b5d2
duongnd2 <nguyenduongnd2@gmail.com>
2020-10-18 00:36:07 +0900

    Commit test trigger jenkins

--- SwiftUIDemoTests/SwiftUIDemoTests.swift
+++ SwiftUIDemoTests/SwiftUIDemoTests.swift
@@ -21,6 +21,7 @@ class SwiftUIDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // add test
     }
 
     func testPerformanceExample() {
